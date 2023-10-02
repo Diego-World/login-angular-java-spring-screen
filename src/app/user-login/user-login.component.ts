3203b4925dbaedbd7b162676877f716096a487c9
Diego-World <contato.diegogouveia@gmail.com>
2023-10-01 22:43:19 -0300

    angular project created and login component created

---
+++ src/app/user-login/user-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-login', // Esse a tag do componente user-login, criado com o comando 'ng g c user-login', que ao ser referenciado no app.component integra ao app.html
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css']
+})
+export class UserLoginComponent {
+
+}
