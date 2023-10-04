5cd136f12f3c1c7827bc9a78527ec3f562659531
Diego-World <contato.diegogouveia@gmail.com>
2023-10-04 19:47:23 -0300

    Criação da classe user para armazenar os dados do formulário

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user-login', // Esse a tag do componente user-login, criado com o comando 'ng g c user-login', que ao ser referenciado no app.component integra ao app.html
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
-export class UserLoginComponent {
+export class UserLoginComponent implements OnInit{
 
+  user:User = new User();
+  
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  userLogin(){
+    console.log(this.user)
+  }
 }
